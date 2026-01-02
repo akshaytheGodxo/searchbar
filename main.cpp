0f3cd8476fa7545e510de4a563db8f487d20430e
akshaytheGodxo <akshaysinghc230@gmail.com>
2026-01-02 22:34:54 +0530

    Refactor to borderless Win32 window with blurred bitmap bg
    Refactored the code to create a custom borderless Win32 popup window with a blurred, stretchable bitmap background loaded from "makimaaaa.bmp" in the current directory. Removed all custom button code and related drawing logic, leaving only a single edit box spanning the top of the window. Added DWM support (dwmapi.lib, <dwmapi.h>) with a stub for blur functionality. Improved resource management by deleting the bitmap on WM_DESTROY and added error handling for image loading. Updated to use Unicode APIs throughout and cleaned up unused variables for clarity.

--- main.cpp
+++ main.cpp
@@ -1,134 +1,104 @@
 #include <windows.h>
-
-#define ID_EDITBOX    1001
-#define ID_BUTTON_OK  1002
+#include <dwmapi.h>
+#include <iostream>
+#pragma comment(lib, "dwmapi.lib")
+#define ID_EDITBOX 1001
+#define _WIN32_WINNT 0x0601
 
 const wchar_t CLASS_NAME[] = L"SearchBar";
+
 HBITMAP g_hBackground = NULL;
-HBITMAP hBGImage;
-HWND hBGhandler;
+HWND g_hEdit = NULL;
 
-void loadImage() {
-    hBGImage = (HBITMAP)LoadImageW(
+void LoadBackground()
+{
+    g_hBackground = (HBITMAP)LoadImageW(
         NULL,
-		L"E:\\Personal Projects\\myengine\\assets\\makimaaaa.bmp",
+        L"makimaaaa.bmp",
         IMAGE_BITMAP,
-        0,0,
-        LR_LOADFROMFILE
+        0, 0,
+        LR_LOADFROMFILE | LR_CREATEDIBSECTION
     );
-}
 
-void DrawCustomButton(LPDRAWITEMSTRUCT lpDrawItem) {
-
-    COLORREF bgColor = RGB(255, 165, 0);     // orange
-    COLORREF textColor = RGB(128, 128, 128);   // grey
-
-    HBRUSH hBrush = CreateSolidBrush(bgColor);
-    FillRect(lpDrawItem->hDC, &lpDrawItem->rcItem, hBrush);
-    DeleteObject(hBrush);
-
-    HFONT hFont = CreateFontW(
-        20, 0, 0, 0,
-        FW_NORMAL, FALSE, FALSE, FALSE,
-        DEFAULT_CHARSET,
-        OUT_DEFAULT_PRECIS,
-        CLIP_DEFAULT_PRECIS,
-        DEFAULT_QUALITY,
-        DEFAULT_PITCH | FF_SWISS,
-        L"Helvetica"
-    );
-    HFONT hOldFont = (HFONT) SelectObject(lpDrawItem->hDC, hFont);
-
-    SetTextColor(lpDrawItem->hDC, textColor);
-    SetBkMode(lpDrawItem->hDC, TRANSPARENT);
-
-    wchar_t text[256];
-    GetWindowTextW(lpDrawItem->hwndItem, text, 256);
-
-    RECT rcText = lpDrawItem->rcItem;
-    DrawTextW(lpDrawItem->hDC, text, -1, &rcText, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
-
-    SelectObject(lpDrawItem->hDC, hOldFont);
-    DeleteObject(hFont);
-
-    if (lpDrawItem->itemState & ODS_FOCUS) {
-        DrawFocusRect(lpDrawItem->hDC, &lpDrawItem->rcItem);
+    if (!g_hBackground)
+    {
+        DWORD err = GetLastError();
+        wchar_t buf[256];
+        wsprintfW(buf, L"LoadImage failed. GetLastError = %lu", err);
+        MessageBoxW(NULL, buf, L"ERROR", MB_OK);
     }
+}
+void EnableBlur(HWND hwnd)
+{
+    DWM_BLURBEHIND bb = {};
+    bb.dwFlags = DWM_BB_ENABLE;
+    bb.fEnable = TRUE;
+    bb.hRgnBlur = NULL;
+
+    DwmEnableBlurBehindWindow(hwnd, &bb);
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    static HFONT hFontButton = NULL;
     switch (msg)
     {
     case WM_CREATE:
     {
-        
 
-
-        LPCREATESTRUCTW pcs = (LPCREATESTRUCTW)lParam;
-        loadImage();
-
-        hBGhandler = CreateWindowW(
-            L"STATIC",
-            NULL,
-            WS_CHILD | WS_VISIBLE | SS_BITMAP,
-            0, 0, 800, 600,
-            NULL,
-            NULL,
-            pcs->hInstance,
-            NULL
-        );
-
-        SendMessageW(hBGhandler,
-            STM_SETIMAGE,
-            IMAGE_BITMAP,
-            (LPARAM)hBGImage);
-
-        HWND hEdit = CreateWindowExW(
+        g_hEdit = CreateWindowExW(
             0,
             L"EDIT",
             L"",
             WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL,
-            3, 10, 780, 25,
+            0, 0, 800, 28,
             hwnd,
             (HMENU)ID_EDITBOX,
-            pcs->hInstance,
+            ((LPCREATESTRUCT)lParam)->hInstance,
             NULL
         );
-        
-
-    }
-    break;
-
-    case WM_DRAWITEM:
-    {
-        LPDRAWITEMSTRUCT lpDrawItem = (LPDRAWITEMSTRUCT)lParam;
-        if (lpDrawItem->CtlID == ID_BUTTON_OK) {
-            DrawCustomButton(lpDrawItem);
-            return TRUE;
-        }
-    }
-    break;
-
-    case WM_DESTROY:
-        PostQuitMessage(0);
         return 0;
+    }
 
     case WM_PAINT:
     {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
 
-        TextOutW(hdc, 10, 10, L"Hello, Win32!", 13);
+        if (g_hBackground)
+        {
+            HDC memDC = CreateCompatibleDC(hdc);
+            HBITMAP oldBmp = (HBITMAP)SelectObject(memDC, g_hBackground);
+
+            BITMAP bm;
+            GetObject(g_hBackground, sizeof(bm), &bm);
+
+            StretchBlt(
+                hdc,
+                0, 0,
+                ps.rcPaint.right,
+                ps.rcPaint.bottom,
+                memDC,
+                0, 0,
+                bm.bmWidth,
+                bm.bmHeight,
+                SRCCOPY
+            );
+
+            SelectObject(memDC, oldBmp);
+            DeleteDC(memDC);
+        }
 
         EndPaint(hwnd, &ps);
         return 0;
     }
-    break;
+
+    case WM_DESTROY:
+        if (g_hBackground)
+            DeleteObject(g_hBackground);
+        PostQuitMessage(0);
+        return 0;
     }
     return DefWindowProc(hwnd, msg, wParam, lParam);
-
 }
 
 int WINAPI wWinMain(
@@ -137,43 +107,41 @@ int WINAPI wWinMain(
     PWSTR,
     int nCmdShow)
 {
-    
-    
-
-    WNDCLASS wc = {};
+    WNDCLASSW wc = {};
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
-    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
-    wc.hbrBackground = NULL;
-	RegisterClass(&wc);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+
+    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+	int screenHeight = GetSystemMetrics(SM_CYSCREEN);
 
 
-    
+    RegisterClassW(&wc);
 
-    HWND hwnd = CreateWindowEx(
+    HWND hwnd = CreateWindowExW(
         0,
         CLASS_NAME,
         L"My Win32 App",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT,
-        800, 600,
-        nullptr,
-        nullptr,
+        WS_POPUP | WS_BORDER | WS_EX_APPWINDOW,
+        (screenWidth / 2) - 750/2, (screenHeight / 2) - 280/2,
+        750, 280,
+        NULL,
+        NULL,
         hInstance,
-        nullptr
+        NULL
     );
-
+    
     if (!hwnd)
         return 0;
 
     ShowWindow(hwnd, nCmdShow);
-   
-    MSG msg = {};
-    while (GetMessage(&msg, nullptr, 0, 0))
+
+    MSG msg;
+    while (GetMessageW(&msg, NULL, 0, 0))
     {
         TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        DispatchMessageW(&msg);
     }
 
     return 0;
