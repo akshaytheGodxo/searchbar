b28a5412541204b620504992971d6f1f551f3143
akshaytheGodxo <akshaysinghc230@gmail.com>
2026-01-06 16:48:18 +0530

    Modernize UI: theming, font, and results list box
    Introduce color constants for consistent dark theme. Add global font and results list box (g_hResult) for improved search display. Refactor StartIndexing to run in a detached thread and index multiple directories. Update SearchFile to populate results list box. Enhance WindowProc to create and style controls with custom font and colors. Handle control coloring for edit and list boxes. Overall, improves usability and visual appeal.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <CommCtrl.h>
 #include <chrono>
+#include <thread>
+#include <fstream>
 #include "Manifest.h"
 #include "FileFinder.h"
 #include "IndexFiles.h"
@@ -14,17 +16,37 @@
 #define ID_EDITBOX 1001
 #define _WIN32_WINNT 0x0601
 
-const wchar_t CLASS_NAME[] = L"SearchBar";
+/* UI Vars */
+constexpr COLORREF BG_COLOR = RGB(30, 30, 30);
+constexpr COLORREF PANEL_COLOR = RGB(38, 38, 38);
+constexpr COLORREF TEXT_COLOR = RGB(230, 230, 230);
+constexpr COLORREF ACCENT_COLOR = RGB(90, 170, 255);
 
+
+
+
+const wchar_t CLASS_NAME[] = L"SearchBar";
+WCHAR placeholderText[] = L"Search anything bozo...";
 HBITMAP g_hBackground = NULL;
 HWND g_hEdit = NULL;
-
+HWND g_hResult = NULL;
+HFONT g_hFont = NULL;
 IndexFiles indexer;
 FileFinder fileFinder;
 
+/* file handling functionalities ---DO NOT TOUCH--- */
+
+
+
 void StartIndexing() {
-    fileFinder.BuildIndexAtStartup(L"C:\\Users\\aksha\\OneDrive\\Pictures", indexer);
-    std::wcout << "Indexing is Done, you may test your input\n";
+
+    std::thread([]{
+        fileFinder.BuildIndexAtStartup(L"C:\\Users\\aksha\\OneDrive\\Pictures", indexer);
+        fileFinder.BuildIndexAtStartup(L"C:\\Users\\aksha\\OneDrive\\Documents", indexer);
+        fileFinder.BuildIndexAtStartup(L"C:\\Users\\aksha\\Downloads", indexer);
+        fileFinder.BuildIndexAtStartup(L"C:\\Users\\aksha\\OneDrive\\Desktop", indexer);
+        std::wcout << "Indexing is Done, you may test your input\n";
+     }).detach();
 }
 
 void SearchFile(const std::wstring& target) {
@@ -32,8 +54,17 @@ void SearchFile(const std::wstring& target) {
     auto iterator = index.find(target);
     if (iterator != index.end()) {
         std::wcout << L"Found: " << iterator->second << L"\n";
+        SendMessageW(g_hResult, LB_RESETCONTENT, 0, 0);
+        SendMessageW(g_hResult, LB_ADDSTRING, 0, (LPARAM)iterator->second.c_str());
     }
+    else {
+        SendMessageW(g_hResult, LB_RESETCONTENT, 0, 0);
+    }
+
 }
+
+
+
 
 DWORD WINAPI MULTITHREADER(LPVOID lpParam) {
     std::wstring* text = static_cast<std::wstring*>(lpParam);
@@ -70,24 +101,53 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
     {
     case WM_CREATE:
     {
+        g_hFont = CreateFontW(
+            18, 0, 0, 0,
+            FW_NORMAL,
+            FALSE, FALSE, FALSE,
+            DEFAULT_CHARSET,
+            OUT_OUTLINE_PRECIS,
+            CLIP_DEFAULT_PRECIS,
+            CLEARTYPE_QUALITY,
+            VARIABLE_PITCH,
+            L"Segoe UI"
+        );
+
+
 
         g_hEdit = CreateWindowExW(
             0,
             L"EDIT",
             L"",
-            WS_BORDER | WS_CHILD | WS_VISIBLE | ES_LEFT  ,
-            0, 0, 800, 28,
+            WS_CHILD | WS_VISIBLE | ES_LEFT | ES_AUTOHSCROLL,
+            20, 20, 710, 36,
             hwnd,
             (HMENU)ID_EDITBOX,
             ((LPCREATESTRUCT)lParam)->hInstance,
             NULL
         );
 
+        SendMessageW(g_hEdit, WM_SETFONT, (WPARAM)g_hFont, TRUE);
+        SendMessageW(g_hEdit, EM_SETMARGINS, EC_LEFTMARGIN | EC_RIGHTMARGIN, MAKELPARAM(12, 12));
         
-        WCHAR placeholderText[] = L"Search anything bozo...";
+        
         SendMessage(g_hEdit, EM_SETCUEBANNER, 1, (LPARAM)placeholderText);
 
-        
+        g_hResult = CreateWindowExW(
+            0,
+            L"LISTBOX",
+            NULL,
+            WS_CHILD | WS_VISIBLE | LBS_NOTIFY | WS_VSCROLL,
+            20, 70, 710, 180,
+            hwnd,
+            NULL,
+            ((LPCREATESTRUCT)lParam)->hInstance,
+            NULL
+        );
+
+        SendMessageW(g_hResult, WM_SETFONT, (WPARAM)g_hFont, TRUE);
+
+
 
         return 0;
     }
@@ -158,6 +218,25 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
         return 0;
     }
 
+    case WM_CTLCOLOREDIT:
+    {
+        HDC hdc = (HDC)wParam;
+        SetTextColor(hdc, TEXT_COLOR);
+        SetBkColor(hdc, PANEL_COLOR);
+        static HBRUSH hBrush = CreateSolidBrush(PANEL_COLOR);
+        return (INT_PTR)hBrush;
+    }
+
+    case WM_CTLCOLORLISTBOX:
+    {
+        HDC hdc = (HDC)wParam;
+        SetTextColor(hdc, TEXT_COLOR);
+        SetBkColor(hdc, PANEL_COLOR);
+        static HBRUSH hBrush = CreateSolidBrush(PANEL_COLOR);
+        return (INT_PTR)hBrush;
+    }
+
+
     case WM_DESTROY:
         if (g_hBackground)
             DeleteObject(g_hBackground);
@@ -185,7 +264,7 @@ int WINAPI wWinMain(
     wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-
+    wc.hbrBackground = CreateSolidBrush(BG_COLOR);
     int screenWidth = GetSystemMetrics(SM_CXSCREEN);
 	int screenHeight = GetSystemMetrics(SM_CYSCREEN);
 
